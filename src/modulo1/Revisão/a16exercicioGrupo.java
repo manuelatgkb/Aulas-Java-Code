697f71d6413b1a0ccf6dbc62344be13526772191
Manuela Briere <manuelatgkb@gmail.com>
2021-09-24 17:28:37 -0300

    com a multiplicação da fatoracao

--- src/modulo1/Revisão/a16exercicioGrupo.java
+++ src/modulo1/Revisão/a16exercicioGrupo.java
@@ -5,16 +5,20 @@ import java.util.Scanner;
 public class a16exercicioGrupo {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
         System.out.println("Digite um número: ");
         int numero = Integer.parseInt(sc.nextLine());
 
         //fatorial de 3 = 3*2*1 = 6
         //fatorial de 5 = 5*4*3*2*1 = 120
+        int fatorial = 1;
 
         for(int i = numero; i>0; i--){
-            System.out.println(i);
+            System.out.printf("%d* ", i);
+            fatorial = fatorial * i;
         }
-        
+        System.out.printf("= %d ", fatorial);
+
         sc.close();
     }
 }
