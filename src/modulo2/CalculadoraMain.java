611a38a6ae6db7543156fa5ab08ed0c4c7563296
Manuela Briere <manuelatgkb@gmail.com>
2021-09-30 17:25:03 -0300

    funciona

--- src/modulo2/CalculadoraMain.java
+++ src/modulo2/CalculadoraMain.java
@@ -1,14 +1,19 @@
 package modulo2;
 
-import modulo1.atividadesPraticas.CalculadoraTaxas;
+//import modulo2.CalculadoraTax;
 
 public class CalculadoraMain {
     public static void main(String[] args) {
         
-    
     CalculadoraTax calctaxas = new CalculadoraTax();
 
-    calctaxas.trBancarias(100.00);
-    calctaxas.taxaSaque(4);
+    double resultadoTransf = calctaxas.trBancarias(100.00);
+
+    System.out.printf("O valor da taxa é:  %.8f \n", resultadoTransf);
+
+    for(int contador = 1; contador <=5; contador ++){
+        double resultadoSaque = calctaxas.taxaSaque(50);
+        System.out.printf("O valor do Saque é:  %.2f \n", resultadoSaque);
+    }
     }
 }
--- src/modulo2/CalculadoraTax.java
+++ src/modulo2/CalculadoraTax.java
@@ -1,19 +1,19 @@
 package modulo2;
 
 public class CalculadoraTax{
-        private double taxaTransf = 0.001; 
+        private double taxaTransf = 0.00001; 
         private double taxaSaque = 1.30; 
         private int qtdSaque; 
 
-        double trBancarias(double valorTransferido){
-            double ValorTaxa = valorTransferido * taxaTransf;
+        double trBancarias(double valorTransf){
+            double ValorTaxa = valorTransf * taxaTransf;
             return ValorTaxa;
         } 
 
         double taxaSaque(double valorSaque){
             qtdSaque++;
-            if(qtdSaque %4 == 0)
-                return valorSaque + taxaSaque;
-            return valorSaque;
+            if(qtdSaque %5 == 0)
+                return taxaSaque;
+            return 0;
         }
 }
