e9d65abdbc70ea03eddfd10aad01d0efec15110f
Manuela Briere <manuelatgkb@gmail.com>
2021-10-01 09:41:15 -0300

    metodo calculo de transferencia

--- src/modulo2/AtividadesIndividuais/ResolucaodaAtividade16/CalculadoraTaxas2.java
+++ src/modulo2/AtividadesIndividuais/ResolucaodaAtividade16/CalculadoraTaxas2.java
@@ -1,5 +1,10 @@
 package modulo2.AtividadesIndividuais.ResolucaodaAtividade16;
 
 public class CalculadoraTaxas2 {
-    
+    private double taxaTransferencia = 0.00001;
+
+    public double CalculadoraTaxaTransferencia(double valor){
+        double valorTaxa = valor * taxaTransferencia; 
+        return valorTaxa;
+    }
 }
