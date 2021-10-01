ec2046d9a43b772f2f81ddeb4a4738ceec1bb08b
Manuela Briere <manuelatgkb@gmail.com>
2021-10-01 14:44:50 -0300

    anotar três métodos

--- src/modulo3/AtividadePratica17/CalculoImpostos.java
+++ src/modulo3/AtividadePratica17/CalculoImpostos.java
@@ -2,4 +2,16 @@ package modulo3.AtividadePratica17;
 
 public class CalculoImpostos {
     
+    public double CalculoISS(){
+
+    }
+
+    public double CalculoIOF(){
+
+    }
+
+    public double CalculoIR(){
+        
+    }
 }
+
--- src/modulo3/AtividadePratica17/CalculoImpostosMain.java
+++ src/modulo3/AtividadePratica17/CalculoImpostosMain.java
@@ -1,5 +1,7 @@
 package modulo3.AtividadePratica17;
 
 public class CalculoImpostosMain {
-    
+    public static void main(String[] args) {
+        
+    }
 }
