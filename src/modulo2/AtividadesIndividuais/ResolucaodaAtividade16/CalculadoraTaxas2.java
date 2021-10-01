2ebb0a780b1da324f046e4806d0645028f059a43
Manuela Briere <manuelatgkb@gmail.com>
2021-10-01 09:43:55 -0300

    criando taxa saque e preparando o for para os cinco saques

--- src/modulo2/AtividadesIndividuais/ResolucaodaAtividade16/CalculadoraTaxas2.java
+++ src/modulo2/AtividadesIndividuais/ResolucaodaAtividade16/CalculadoraTaxas2.java
@@ -2,9 +2,16 @@ package modulo2.AtividadesIndividuais.ResolucaodaAtividade16;
 
 public class CalculadoraTaxas2 {
     private double taxaTransferencia = 0.00001;
+    private double taxaSaque = 1.3;
 
     public double CalculadoraTaxaTransferencia(double valor){
         double valorTaxa = valor * taxaTransferencia; 
+        return valorTaxa;
+    }
+
+    public double calculaTaxaSaque(double valor){
+        double valorTaxa = taxaSaque;
+        
         return valorTaxa;
     }
 }
