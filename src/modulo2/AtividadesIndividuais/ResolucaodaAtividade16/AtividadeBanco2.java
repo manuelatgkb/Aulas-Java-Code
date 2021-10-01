bae79f02e3d89ae61225d1b48a1d6635ad4246f3
Manuela Briere <manuelatgkb@gmail.com>
2021-10-01 09:52:10 -0300

    criandop main e variáveis intemediárias para executar os metodos de transferencia e saque

--- src/modulo2/AtividadesIndividuais/ResolucaodaAtividade16/AtividadeBanco2.java
+++ src/modulo2/AtividadesIndividuais/ResolucaodaAtividade16/AtividadeBanco2.java
@@ -1,5 +1,16 @@
 package modulo2.AtividadesIndividuais.ResolucaodaAtividade16;
 
 public class AtividadeBanco2 {
-    
+    public static void main(String[] args) {
+        CalculadoraTaxas2 calcTaxas = new CalculadoraTaxas2();
+        double taxaT1 = calcTaxas.CalculadoraTaxaTransferencia(1000);
+        double taxaT2 = calcTaxas.CalculadoraTaxaTransferencia(500);
+
+        double taxaS1 = calcTaxas.calculaTaxaSaque(50);
+        double taxaS2 = calcTaxas.calculaTaxaSaque(50);
+        double taxaS3 = calcTaxas.calculaTaxaSaque(50);
+        double taxaS4 = calcTaxas.calculaTaxaSaque(50);
+        double taxaS5 = calcTaxas.calculaTaxaSaque(50);
+
+    }
 }
