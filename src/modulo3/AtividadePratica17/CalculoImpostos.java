4c627858085c81fc8e403b96ff111bf55cb2413a
Manuela Briere <manuelatgkb@gmail.com>
2021-10-01 14:51:15 -0300

    variáveis privadas com o valor de cada imposto

--- src/modulo3/AtividadePratica17/CalculoImpostos.java
+++ src/modulo3/AtividadePratica17/CalculoImpostos.java
@@ -1,6 +1,11 @@
 package modulo3.AtividadePratica17;
 
 public class CalculoImpostos {
+
+    //A classe deve manter em variáveis privadas o valor de cada imposto
+    private double ISS = 0.00003;
+    private double IOF = 6.38;
+    private double IR;
     
     public double CalculoISS(){
 
