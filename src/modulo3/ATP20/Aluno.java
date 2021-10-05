81b1c7559718d6f047f5c1ee0784325c3af00b86
Manuela Briere <manuelatgkb@gmail.com>
2021-10-05 15:23:19 -0300

    construção de metodos set e get

--- src/modulo3/ATP20/Aluno.java
+++ src/modulo3/ATP20/Aluno.java
@@ -3,4 +3,17 @@ package modulo3.ATP20;
 public class Aluno extends Pessoa{
     private String curso;
     private String turma;
+
+    public String getCurso() {
+        return curso;
+    }
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
+    public void setTurma(String turma) {
+        this.turma = turma;
+    }
+    public String getTurma() {
+        return turma;
+    }
 }
