cc5c50c4f556372a09d273fe9a61fb3b43941264
Manuela Briere <manuelatgkb@gmail.com>
2021-10-05 14:23:51 -0300

    heranças

---
+++ src/modulo3/aula14/P2/Conta.java
@@ -0,0 +1,27 @@
+package modulo3.aula14.P2;
+
+public class Conta {
+    private String agencia;
+    private String numero;
+    private double saldo;
+
+    
+    public void setAgencia(String agencia) {
+        this.agencia = agencia;
+    }
+    public String getAgencia() {
+        return agencia;
+    }
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+    public String getNumero() {
+        return numero;
+    }
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+    public double getSaldo() {
+        return saldo;
+    }
+}
---
+++ src/modulo3/aula14/P2/ContaPoupanca.java
@@ -0,0 +1,13 @@
+package modulo3.aula14.P2;
+
+public class ContaPoupanca extends Conta {
+        private double rendimento;
+
+    public void setRendimento(double rendimento){
+        this.rendimento = rendimento;
+        
+    }
+    public double getRendiemnto() {
+        return rendimento;
+    }       
+}
