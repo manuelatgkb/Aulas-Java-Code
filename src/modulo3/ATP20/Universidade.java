8242ae804b89fb3075af9c1565c01089449865db
Manuela Briere <manuelatgkb@gmail.com>
2021-10-05 15:46:42 -0300

    inserir valores e imprimir

--- src/modulo3/ATP20/Universidade.java
+++ src/modulo3/ATP20/Universidade.java
@@ -1,5 +1,45 @@
 package modulo3.ATP20;
 
 public class Universidade {
-    
+    public static void main(String[] args) {
+        Funcionario f = new Funcionario(); 
+        f.setNome("Maykon");
+        f.setSobrenome("Diego");
+        f.setMatricula(99999.000);
+        f.setDepartamento("Formação");
+        f.setSalario(10000.00);
+
+        Coordenador c = new Coordenador();
+        c.setNome("Ana");
+        c.setSobrenome("Souza");
+        c.setMatricula(88888.000);
+        c.setDepartamento("Coordenação");
+        c.setSalario(20000.00);
+
+        Aluno a = new Aluno();
+        a.setNome("Clarissa");
+        a.setSobrenome("Souza");
+        a.setMatricula(292929.99);
+        a.setCurso("Biologia");
+        a.setTurma("b");
+
+
+        System.out.printf("Nome : , %s ", f.getNome());
+        System.out.printf("Sobrenome :, %s ", f.getSobrenome());
+        System.out.printf("Matricula :, %d", f.getMatricula());
+        System.out.printf("Departamento :, %s", f.getDepartamento());
+        System.out.printf("Salário: , %d", f.getSalario());
+
+        System.out.printf("Nome : , %s ", c.getNome());
+        System.out.printf("Sobrenome :, %s ", c.getSobrenome());
+        System.out.printf("Matricula :, %d", c.getMatricula());
+        System.out.printf("Departamento : %s", c.getDepartamento());
+        System.out.printf("Salário: , %d", c.getSalario());
+
+        System.out.printf("Nome : , %s ", a.getNome());
+        System.out.printf("Sobrenome :, %s ", a.getSobrenome());
+        System.out.printf("Matricula :, %d", a.getMatricula());
+        System.out.printf("Curso : %s", a.getCurso());
+        System.out.printf("Turma : , %s", a.getTurma());
+    }
 }
