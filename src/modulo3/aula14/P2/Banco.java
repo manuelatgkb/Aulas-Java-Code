aa0ca63f2500ae12635139a4cec43fa1800e1024
Manuela Briere <manuelatgkb@gmail.com>
2021-10-05 14:23:39 -0300

    Herançcas parte 2

---
+++ src/modulo3/aula14/P2/Banco.java
@@ -0,0 +1,8 @@
+package modulo3.aula14.P2;
+
+public class Banco {
+    public static void main(String[]args){
+        ContaPoupanca contaP = new ContaPoupanca();
+        contaP.getSaldo();
+    }
+}
