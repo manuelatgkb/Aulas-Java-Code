1a6d86d2011b06ebcac1426da30d4a6956ade762
Manuela Briere <manuelatgkb@gmail.com>
2021-10-05 15:12:25 -0300

    m

--- src/modulo3/aula14/P2/Banco.java
+++ src/modulo3/aula14/P2/Banco.java
@@ -2,7 +2,7 @@ package modulo3.aula14.P2;
 
 public class Banco {
     public static void main(String[]args){
+
         ContaPoupanca contaP = new ContaPoupanca();
-        contaP.getSaldo();
     }
 }
