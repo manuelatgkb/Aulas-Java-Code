18055171db40dff79af07457f477fbfae5dc18df
Manuela Briere <manuelatgkb@gmail.com>
2021-10-05 11:48:08 -0300

    sem erros

--- src/modulo3/ATP19/Numeros.java
+++ src/modulo3/ATP19/Numeros.java
@@ -1,23 +1,21 @@
 package modulo3.ATP19;
 
+import java.util.Scanner;
+
 public class Numeros {
    public static void main(String[] args) {
-        int[] numeros = new int[10];
 
-        numeros[0] = 1;
-        numeros[1] = 2;
-        numeros[2] = 3;
-        numeros[3] = 4;
-        numeros[4] = 5;
-        numeros[5] = 6;
-        numeros[6] = 7;
-        numeros[7] = 8;
-        numeros[8] = 9;
-        numeros[9] = 10;
+    Scanner sc = new Scanner(System.in);
+    int[] arrayInteiros = new int[10];
 
-        // percorrendo o array através do laço for
-        for(int i=0; i<numeros.length; i++){
-            System.out.println(numeros[i]);
-        }
+    for (int i = 0; i < arrayInteiros.length; i++) {
+        System.out.printf("Digite o valor do %dº número inteiro: ", i+1);
+        arrayInteiros[i] = Integer.parseInt(sc.nextLine());
     }
+
+    for (int j : arrayInteiros) {
+        System.out.printf("Valor: %d\n", j);         
+    }
+
+}
 } 
