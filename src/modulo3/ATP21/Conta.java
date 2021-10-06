9f13935b17e47aab0b0b3ada96e82caf6429ca3e
Manuela Briere <manuelatgkb@gmail.com>
2021-10-06 13:26:35 -0300

    criação do método de transferência na classe conta

---
+++ src/modulo3/ATP21/Conta.java
@@ -0,0 +1,23 @@
+package modulo3.ATP21;
+
+public class Conta {
+    private double saldo; 
+    private String codigo_cliente;
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+    public double getSaldo() {
+        return saldo;
+    }
+    public void setCodigo_cliente(String codigo_cliente) {
+        this.codigo_cliente = codigo_cliente;
+    }
+    public String getCodigo_cliente() {
+        return codigo_cliente;
+    }
+    public void transferencia(double valor){
+        double saldo_final = saldo - valor;
+        saldo = saldo_final;
+    }
+}
---
+++ src/modulo3/ATP21/ContaCorrente.java
@@ -0,0 +1,11 @@
+package modulo3.ATP21;
+
+public class ContaCorrente extends Conta{
+    private double taxa_transferencia = 10.50;
+    private double taxa_manutencao = 2.50;
+    private int numero_transferencias = 0;
+
+
+
+
+}
