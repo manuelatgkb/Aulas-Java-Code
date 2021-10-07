c942b0a5440f650f04b92f13bfbcb184c53fb9b1
Manuela Briere <manuelatgkb@gmail.com>
2021-10-07 15:52:39 -0300

    Criação da classe aluno, seus atributos e seus gest e sets

---
+++ src/modulo3/ATP22/P1/Aluno.java
@@ -0,0 +1,33 @@
+package modulo3.ATP22.P1;
+
+public class Aluno {
+   public String nome; 
+   public String sobrenome;
+   public int idade;
+   public double matricula;
+
+   public void setNome(String nome) {
+       this.nome = nome;
+   }
+   public String getNome() {
+       return this.nome;
+   }
+   public void setSobrenome(String sobrenome) {
+       this.sobrenome = sobrenome;
+   }
+   public String getSobrenome() {
+       return this.sobrenome;
+   }
+   public void setIdade(int idade) {
+       this.idade = idade;
+   }
+   public int getIdade() {
+       return this.idade;
+   }
+   public void setMatricula(double matricula) {
+       this.matricula = matricula;
+   }
+   public double getMatricula() {
+       return this.matricula;
+   }
+}
