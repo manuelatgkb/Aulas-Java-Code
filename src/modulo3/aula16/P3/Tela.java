8c884596b507c7762e8ae703e77187487f4589e9
Manuela Briere <manuelatgkb@gmail.com>
2021-10-07 14:37:39 -0300

    b

--- src/modulo3/aula16/P3/Tela.java
+++ src/modulo3/aula16/P3/Tela.java
@@ -2,12 +2,12 @@ package modulo3.aula16.P3;
 
 public class Tela {
     public static void main(String[] args) {
-        Pessoa p1 = new Pessoa();
+       // Pessoa p1 = new Pessoa();
 
-        p1.setNome("Maykon");
-        p1.setSobrenome("Granemann");
+       // p1.setNome("Maykon");
+      //  p1.setSobrenome("Granemann");
 
-        String nome = p1.getNome();
-        String sobrenome = p1.getSobrenome();
-    }
+       // String nome = p1.getNome();
+       // String sobrenome = p1.getSobrenome();
+   }
 }
