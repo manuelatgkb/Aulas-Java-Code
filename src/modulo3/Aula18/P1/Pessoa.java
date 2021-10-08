c5d6d016fd287338a301f026418566420583680c
Manuela Briere <manuelatgkb@gmail.com>
2021-10-08 15:55:59 -0300

    n

---
+++ src/modulo3/Aula18/P1/Pessoa.java
@@ -0,0 +1,13 @@
+package modulo3.Aula18.P1;
+
+public class Pessoa {
+    private String nomeCompleto;
+
+    public void setNomeCompleto(String nomeCompleto) {
+        this.nomeCompleto = nomeCompleto;
+    }
+
+    public String getNomeCompleto() {
+        return nomeCompleto;
+    }
+}
