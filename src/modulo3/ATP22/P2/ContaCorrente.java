338de03f3f54ad0fa73fd4c831779454c20be67b
Manuela Briere <manuelatgkb@gmail.com>
2021-10-08 11:53:33 -0300

    corrigido

--- src/modulo3/ATP22/P2/ContaCorrente.java
+++ src/modulo3/ATP22/P2/ContaCorrente.java
@@ -2,12 +2,15 @@ package modulo3.ATP22.P2;
 
 public class ContaCorrente {
     private double saldo;
+    private double saldoInicial;
+    private double saques;
     private double taxaSaque = 1.50;
     private double codigoCliente;
 
      //Crie um método Construtor na classe Conta Corrente que 
      //receba o valor do saldo e do código do cliente;
     public ContaCorrente(double saldo, double codigoCliente){
+        this.saldoInicial = 9000.99;
         this.saldo = saldo;
         this.codigoCliente = codigoCliente;
     }
@@ -21,7 +24,7 @@ public class ContaCorrente {
 
     @Override
     public String toString(){
-        return "Saldo: " + this.saldo + " \n Código Cliente: " + this.codigoCliente + "\n Taxa de Saque: " + this.taxaSaque;
+        return "O Saldo inicial é: " + this.saldoInicial + "\nSaldo: " + this.saldo + " \n Código Cliente: " + this.codigoCliente + "\n Taxa de Saque: " + this.taxaSaque;
     
     }    
 }
