60c78cb431c8d2e33409f12013e1f82906f6a2e2
Manuela Briere <manuelatgkb@gmail.com>
2021-10-08 12:04:05 -0300

    corrigido

--- src/modulo3/ATP22/P3/Calculadora.java
+++ src/modulo3/ATP22/P3/Calculadora.java
@@ -3,20 +3,19 @@ package modulo3.ATP22.P3;
 public class Calculadora {
 
     public int soma1(int v1, int v2){
-        System.out.printf("Soma com 2 inteiros");
+        //System.out.printf("Soma com 2 inteiros");
         return v1 + v2;
     }
     public double soma2(double v1, double v2){
-        System.out.printf("Soma com 2 doubles");
-        return v1+ v2;
+        //System.out.printf("Soma com 2 doubles");
+        return v1 + v2;
     }
     public float soma3(float v1, float v2, float v3){
-        System.out.println("Soma com 3 float");
-        float rs3 = v1 + v2 + v3;
-        return rs3;
+        //System.out.println("Soma com 3 float");
+        return  v1 + v2 + v3;
     }
     public long soma4(long v1, long v2, long v3){
-        System.out.println("Soma com 3 long");
+        //System.out.println("Soma com 3 long");
         return v1+ v2 + v3;
     }
 }
--- src/modulo3/ATP22/P3/Tela.java
+++ src/modulo3/ATP22/P3/Tela.java
@@ -5,8 +5,15 @@ public class Tela {
     public static void main(String[] args) {
         Calculadora calc = new Calculadora();
         int s1 = calc.soma1(10, 20); 
-        double s2 = calc.soma2(11, 22);
-        float s3 = calc.soma3( 23, 34, 33); 
+        double s2 = calc.soma2(11.1, 22.8);
+        float s3 = calc.soma3( 23.2f, 34.8f, 33.10f); 
         long s4 = calc.soma4( 34, 44, 33); 
+        
+//imprimir os valores
+        System.out.println(s1);
+        System.out.println(s2);
+        System.out.println(s3);
+        System.out.println(s4);
+
     }
 }
