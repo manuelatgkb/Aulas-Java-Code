2b1aadc4c9497fca5b139b75ee77884c878d3f27
Manuela Briere <manuelatgkb@gmail.com>
2021-10-08 16:14:28 -0300

    Crud

---
+++ src/modulo3/Aula18/P2/Aluno.java
@@ -0,0 +1,20 @@
+package modulo3.Aula18.P2;
+
+public class Aluno {
+    private String nome;
+    private String idade; 
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getNome() {
+        return nome;
+    }
+
+    public void setIdade(String idade) {
+        this.idade = idade;
+    }
+    public String getIdade() {
+        return idade;
+    }
+}
---
+++ src/modulo3/Aula18/P2/Principal.java
@@ -0,0 +1,13 @@
+package modulo3.Aula18.P2;
+
+
+public class Principal {
+   public static void main(String[] args) {
+       //CRUD
+       
+       //create
+       //update
+       //read
+       //delete
+   } 
+}
