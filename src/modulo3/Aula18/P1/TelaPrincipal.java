958d94248d5a6c0826a61bc5bcead345211d8274
Manuela Briere <manuelatgkb@gmail.com>
2021-10-08 16:03:50 -0300

    super Array

--- src/modulo3/Aula18/P1/TelaPrincipal.java
+++ src/modulo3/Aula18/P1/TelaPrincipal.java
@@ -1,5 +1,22 @@
 package modulo3.Aula18.P1;
 
+import java.util.ArrayList;
+
 public class TelaPrincipal {
-    
+    public static void main(String[] args) {
+        Pessoa p1 = new Pessoa();
+        p1.setNomeCompleto("Inês Brasil");
+
+        Pessoa p2 = new Pessoa();
+        p2.setNomeCompleto("Joelma Calypso");
+
+        Pessoa p3 = new Pessoa();
+        p3.setNomeCompleto("Michael Douglas");
+
+        //Pessoa[] pessoas = new Pessoa[3];
+        
+        ArrayList<Pessoa> ap = new ArrayList<Pessoa>();
+        
+
+    }
 }
