85fded215b4590cf0a82a21caef61d48fae6fb1c
Manuela Briere <manuelatgkb@gmail.com>
2021-10-13 16:47:56 -0300

    boolean igual = nome.toUpperCase().equals(nome2.toUpperCase());

--- src/modulo3/Aula19/P1/Main.java
+++ src/modulo3/Aula19/P1/Main.java
@@ -4,16 +4,13 @@ public class Main {
     public static void main(String[] args) {
         String nome = "Inêz";
         String nome2 = "inêz";
-        nome = nome.toUpperCase();
-        nome2 = nome2.toUpperCase();
+       // nome = nome.toUpperCase();
+        //nome2 = nome2.toUpperCase();
 
-        boolean igual = nome.equals(nome2);
+        boolean igual = nome.toUpperCase().equals(nome2.toUpperCase());
     //não estou salvando a variável
         System.out.println(igual);
         System.out.println(nome);
         System.out.println(nome2);
-
-
-        
     }
 }
