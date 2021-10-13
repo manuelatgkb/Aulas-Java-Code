7d5dd2c23600f020cbd913e0ed532575bbc877b1
Manuela Briere <manuelatgkb@gmail.com>
2021-10-13 16:42:30 -0300

    equals()

---
+++ src/modulo3/Aula19/P1/Main.java
@@ -0,0 +1,13 @@
+package modulo3.Aula19.P1;
+
+public class Main {
+    public static void main(String[] args) {
+        String nome = "Inêz";
+        System.out.println(nome.equals("a"));
+        //case sensitive
+        System.out.println(nome.equals("Inêz"));
+        System.out.println(nome.equals("inêz"));
+
+
+    }
+}
