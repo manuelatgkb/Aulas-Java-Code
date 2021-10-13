4ff55e10d57560a123e8b838d988d442e5f4ed09
Manuela Briere <manuelatgkb@gmail.com>
2021-10-13 16:44:56 -0300

    toUpperCase

--- src/modulo3/Aula19/P1/Main.java
+++ src/modulo3/Aula19/P1/Main.java
@@ -3,11 +3,13 @@ package modulo3.Aula19.P1;
 public class Main {
     public static void main(String[] args) {
         String nome = "Inêz";
-        System.out.println(nome.equals("a"));
-        //case sensitive
-        System.out.println(nome.equals("Inêz"));
-        System.out.println(nome.equals("inêz"));
+        String nome2 = "inêz";
+        nome = nome.toUpperCase();
+        nome2 = nome2.toUpperCase();
 
-
+        boolean igual = nome.equals(nome2);
+    
+        System.out.println(nome.equals(igual));
+        
     }
 }
