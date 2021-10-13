c324ff428f88e8d6653f5178803e34c2d164c3b8
Manuela Briere <manuelatgkb@gmail.com>
2021-10-13 17:48:15 -0300

    Conversoes

---
+++ src/modulo3/Aula19/P1/MainEquals.java
@@ -0,0 +1,16 @@
+package modulo3.Aula19.P1;
+
+public class MainEquals {
+   public static void main(String[] args) {
+    Filme f1 = new Filme();
+    f1.titulo = "Poderoso chefão";
+    f1.anoLancamento = 1999;
+    f1.genero = "Crime";
+
+    Filme f2 = new Filme();
+    f2.titulo = "Titanic";
+    f2.anoLancamento = 1994;
+    f2.genero = "Drama";
+
+   } 
+}
---
+++ src/modulo3/Aula19/P1/MainEqualsObject.java
@@ -0,0 +1,18 @@
+package modulo3.Aula19.P1;
+
+public class MainEqualsObject {
+    public static void main(String[] args) {
+        Filme f1 = new Filme();
+        f1.titulo = "Poderoso chefão";
+        f1.anoLancamento = 1999;
+        f1.genero = "Crime";
+
+        Filme f2 = new Filme();
+        f2.titulo = "Titanic";
+        f2.anoLancamento = 1994;
+        f2.genero = "Drama";
+
+        boolean igual = f1.equals(f2);
+        System.out.println(igual);
+    }
+}
