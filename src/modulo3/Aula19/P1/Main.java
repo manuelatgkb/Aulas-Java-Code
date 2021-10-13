6ecce67407361c962140f9b9dc3230f6c7e716ab
Manuela Briere <manuelatgkb@gmail.com>
2021-10-13 17:17:42 -0300

    replace e substring

--- src/modulo3/Aula19/P1/Main.java
+++ src/modulo3/Aula19/P1/Main.java
@@ -6,11 +6,17 @@ public class Main {
         String nome2 = "inêz";
        // nome = nome.toUpperCase();
         //nome2 = nome2.toUpperCase();
+        nome = nome.replace('s', 'z');
+        nome2 = nome.substring(0,2);
+        char a = 'a';
 
         boolean igual = nome.toUpperCase().equals(nome2.toUpperCase());
+        if(nome.equals(nome2)){
+
+        }
     //não estou salvando a variável
-        System.out.println(igual);
-        System.out.println(nome);
-        System.out.println(nome2);
+        //System.out.println(igual);
+        //System.out.println(nome);
+        //System.out.println(nome2);
     }
 }
