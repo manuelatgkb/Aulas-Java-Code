68cea013d472a3b05236beb4b75947ffd6b5f4cf
Manuela Briere <manuelatgkb@gmail.com>
2021-10-14 17:45:36 -0300

    final

---
+++ src/modulo3/ATP23/controller/LivrosController.java
@@ -0,0 +1,36 @@
+package modulo3.ATP23.controller;
+import java.util.ArrayList;
+
+import modulo3.ATP23.model.Livros;
+
+
+
+    //controller
+
+    public class LivrosController {
+        private ArrayList<Livros> livros = new ArrayList<Livros>();
+    
+    //create
+
+        public void salvar(Livros livro){
+            livros.add(livro);
+        }
+     //read
+
+        public ArrayList<Livros> listar(){
+            return livros;
+        }
+
+        
+    //Update
+        public void alterar(Livros livro){
+            for (int i = 0; i < livros.size(); i++) {
+        }
+    }
+
+    //delete
+        public void deletar(Livros livro){
+        this.livros.remove(livro);
+    }
+}
+    
