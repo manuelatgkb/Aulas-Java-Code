e76497b6f077addfc7408582388bc32a23a328cd
Manuela Briere <manuelatgkb@gmail.com>
2021-10-15 14:57:00 -0300

    override classe neta

--- src/modulo3/Aula21/Neta.java
+++ src/modulo3/Aula21/Neta.java
@@ -2,4 +2,10 @@ package modulo3.Aula21;
 
 public class Neta extends Filha{
     public String celular;
+
+    @Override
+    public void Falar() {
+        System.out.println("Classe Neta");        
+        super.Falar();
+    }
 }
