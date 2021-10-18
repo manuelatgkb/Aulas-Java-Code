322451e7c37c6dbf977cbc85bf47766724e68eb3
Manuela Briere <manuelatgkb@gmail.com>
2021-10-18 11:37:48 -0300

    aula 22

---
+++ src/modulo4/Aula22/P2/Pessoa.java
@@ -0,0 +1,8 @@
+public class Pessoa {
+    public String nome;
+
+    @Override
+    public String toString(){
+        return this.nome;
+    }
+}
---
+++ src/modulo4/Aula22/P2/PessoaFisica.java
@@ -0,0 +1,3 @@
+public class PessoaFisica extends Pessoa{
+    public String cpf;
+}
