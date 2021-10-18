2d8f1cc5c566397e49ecd5412dfff4ce2157077d
Manuela Briere <manuelatgkb@gmail.com>
2021-10-18 16:41:48 -0300

    criação de override nas tres classes

--- src/modulo4/ATP25/Desktop.java
+++ src/modulo4/ATP25/Desktop.java
@@ -4,4 +4,9 @@ public class Desktop extends Computador{
     public String tamGabinete;
     public String monitor;
     public String fonte;
+
+    @Override
+    public String toString(){
+        return this.tamGabinete + "," + this.monitor + "," + this.fonte;
+    }
 }
