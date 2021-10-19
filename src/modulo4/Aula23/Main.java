c8fa53f5d77c6128ed86d7b8c14dc499f85132b9
Manuela Briere <manuelatgkb@gmail.com>
2021-10-19 11:43:05 -0300

    reorganiza

--- src/modulo4/Aula23/Main.java
+++ src/modulo4/Aula23/Main.java
@@ -4,6 +4,9 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
+        //ArrayList<Object> a = new ArrayList<Object>();
+        //a.add();
+
         Pessoa p1 = new Pessoa();
         p1.nome = "Jojo";
         p1.sobrenome= "DoCalypso";
@@ -13,8 +16,17 @@ public class Main {
         c1.modelo = "BrasiliaAmarela";
 
         Dados dados = new Dados();
+
+        System.out.println(dados.contains(p1));
+        System.out.println(dados.contains(c1));
+
         System.out.println(dados.add(p1));
         System.out.println(dados.add(c1));
+
+        System.out.println(dados.contains(p1));
+        System.out.println(dados.contains(c1));
+
+
         System.out.println(dados.add(p1));
         System.out.println(dados.add(c1));
         System.out.println(dados.add(p1));
