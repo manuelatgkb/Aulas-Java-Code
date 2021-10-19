07ce7f4a8271f615642cb08214e0d17a00698dd7
Manuela Briere <manuelatgkb@gmail.com>
2021-10-19 10:29:40 -0300

    for (int i = 0; i < dados.length; i++) {                 dados2[i] = dados[i];             }

--- src/modulo4/Aula23/Dados.java
+++ src/modulo4/Aula23/Dados.java
@@ -1,19 +1,24 @@
 package modulo4.Aula23;
 
-
 public class Dados {
     int posicao = 0;
     Object[] dados = new Object[4];
 
     public String add(Object obj){
         String mensagem;
+
         if(posicao < dados.length){
-        dados[posicao] = obj;
-        posicao++;
-        mensagem = "Salvo com sucesso";
+            dados[posicao] = obj;
+            posicao ++;
         }else{
             Object[] dados2 = new Object[dados.length*2];
+
+            for (int i = 0; i < dados.length; i++) {
+                dados2[i] = dados[i];
+            }
+            dados = dados2;
     }
+    mensagem = "Salvo com sucesso";
     return mensagem;
 }
 }
