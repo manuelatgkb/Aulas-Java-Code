6e2d3fab82b0399deace969749c55abaa44c5622
Manuela Briere <manuelatgkb@gmail.com>
2021-10-19 09:57:27 -0300

    arrays

--- src/modulo4/Aula23/Main.java
+++ src/modulo4/Aula23/Main.java
@@ -1,7 +1,13 @@
 package modulo4.Aula23;
 
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        
+        int n = 10;
+        //criação de arrays
+        int[] numeros = new int[10];
+        //aray list
+        ArrayList<Double> numeros2 = new ArrayList<Double>();
     }
 }
