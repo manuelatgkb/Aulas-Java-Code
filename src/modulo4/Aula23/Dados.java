b9f0ecbae4d6feae4602cf76cb39d815cd883b5b
Manuela Briere <manuelatgkb@gmail.com>
2021-10-19 10:22:05 -0300

    Object[] dados2 = new Object[dados.length*2];

--- src/modulo4/Aula23/Dados.java
+++ src/modulo4/Aula23/Dados.java
@@ -3,10 +3,17 @@ package modulo4.Aula23;
 
 public class Dados {
     int posicao = 0;
-    Object[] dados = new Object[10];
+    Object[] dados = new Object[4];
 
-    public void add(Object obj){
+    public String add(Object obj){
+        String mensagem;
+        if(posicao < dados.length){
         dados[posicao] = obj;
         posicao++;
+        mensagem = "Salvo com sucesso";
+        }else{
+            Object[] dados2 = new Object[dados.length*2];
     }
+    return mensagem;
+}
 }
