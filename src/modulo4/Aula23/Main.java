8069b498bc7632e676082258418d1fc4865c31fa
Manuela Briere <manuelatgkb@gmail.com>
2021-10-19 10:00:58 -0300

    criaçao de array Object, que aceita todas as classes

--- src/modulo4/Aula23/Main.java
+++ src/modulo4/Aula23/Main.java
@@ -4,10 +4,25 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        int n = 10;
+        Object[] dados = new Object[10];
+        Pessoa p1 = new Pessoa();
+        p1.nome = "Jojo";
+        p1.sobrenome= "DoCalypso";
+
+        Carro c1 = new Carro();
+        c1.marca = "VW";
+        c1.modelo = "BrasiliaAmarela";
+
+        dados[0] = p1;
+        dados[1] = c1;
+
+
+
+        /*int n = 10;
         //criação de arrays
         int[] numeros = new int[10];
         //aray list
         ArrayList<Double> numeros2 = new ArrayList<Double>();
+        */
     }
 }
