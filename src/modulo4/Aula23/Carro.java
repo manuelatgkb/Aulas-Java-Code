016afc6cb445d1f7dd6ffbcffe997ed427d302ba
Manuela Briere <manuelatgkb@gmail.com>
2021-10-19 12:08:21 -0300

    equals

--- src/modulo4/Aula23/Carro.java
+++ src/modulo4/Aula23/Carro.java
@@ -5,7 +5,7 @@ public class Carro {
     public String modelo;
 
     @Override
-    public boolean equals(Object obj){}
+    public boolean equals(Object obj){
         Carro outroCarro = (Carro)obj;
         if( this.marca.equals(outroCarro.marca) && this.modelo.equals(outroCarro.modelo)){
             return true;
--- src/modulo4/Aula23/Main.java
+++ src/modulo4/Aula23/Main.java
@@ -1,6 +1,6 @@
 package modulo4.Aula23;
 
-import java.util.ArrayList;
+//import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
