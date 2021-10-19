10651197f33978be7454bc1e9c626012035dcaa2
Manuela Briere <manuelatgkb@gmail.com>
2021-10-19 14:21:58 -0300

    v

--- src/modulo4/ATP26/Pessoa.java
+++ src/modulo4/ATP26/Pessoa.java
@@ -4,4 +4,9 @@ public class Pessoa {
     public String nome;
     public String sobrenome;
     public String idade;
+
+    @Override
+    public String toString(){
+        this.nome +  "," + this.sobrenome + "," + this.idade;
+    }
 }
