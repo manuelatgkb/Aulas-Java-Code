9ba2851453a1a2a25c6a100865fcf48d3f2e2804
Manuela Briere <manuelatgkb@gmail.com>
2021-10-19 14:37:50 -0300

    _  Crie uma inicialização do array para 5 posições dentro do do método construtor da classe.

--- src/modulo4/ATP26/Dados.java
+++ src/modulo4/ATP26/Dados.java
@@ -1,6 +1,8 @@
 package modulo4.ATP26;
 
 public class Dados {
+    public Dados(){
     //Crie uma classe Dados com um atributo privado do tipo array de Object.
     Object[] obj = new Object[5];
+    }
 }
