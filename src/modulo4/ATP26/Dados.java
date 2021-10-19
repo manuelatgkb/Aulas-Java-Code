d401e5809d07871b34d2fe2d35296cf09213dedb
Manuela Briere <manuelatgkb@gmail.com>
2021-10-19 14:51:30 -0300

    _  Crie um método para adicionar elementos ao array e que permita receber tanto objetos da classe pessoa quanto objetos da classe Aluno. Caso o array já tenha todas as posições ocupadas, deve ser adicionado mais 5 posições ao array.

--- src/modulo4/ATP26/Dados.java
+++ src/modulo4/ATP26/Dados.java
@@ -1,8 +1,32 @@
 package modulo4.ATP26;
 
 public class Dados {
-    public Dados(){
     //Crie uma classe Dados com um atributo privado do tipo array de Object.
-    Object[] obj = new Object[5];
+    Object[] dados;
+    int posicaoAtual = 0;
+    int tamanhoAtual = 5;
+
+    public Dados(){
+    this.dados = new Object[5];
     }
+
+    public String add(Object obj){
+        String mensagem;
+        if(posicaoAtual < dados.length){
+            dados[posicaoAtual] = obj;
+            posicaoAtual ++;
+        }else{
+            tamanhoAtual = tamanhoAtual + 5;
+            Object[] dados2 = new Object[tamanhoAtual];
+
+            for (int i = 0; i < dados.length; i++) {
+                dados2[i] = dados[i];
+            }
+            dados = dados2;
+        }
+    mensagem = "Salvo com sucesso";
+    return mensagem;
+    }
+
+
 }
