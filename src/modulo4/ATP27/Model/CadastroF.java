f42855f97970d93cd633ee062704e93b036be641
Manuela Briere <manuelatgkb@gmail.com>
2021-10-20 15:22:50 -0300

    Crie um sistema de cadastro de Filmes e Séries. _  O sistema deve utilizar o padrão de arquitetura MVC _  Os modelos devem possuir um id e outros atributos públicos de sua escolha.

---
+++ src/modulo4/ATP27/Model/CadastroF.java
@@ -0,0 +1,7 @@
+package modulo4.ATP27.Model;
+
+public class CadastroF {
+    public int id;
+    public String titulo;
+    public String ano;
+}
---
+++ src/modulo4/ATP27/Model/CadastroS.java
@@ -0,0 +1,5 @@
+package modulo4.ATP27.Model;
+
+public class CadastroS extends CadastroF{
+    public int id;
+}
