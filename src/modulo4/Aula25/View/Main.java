d1c1e2536f4a64604698f094cd0b793efc0bdcb3
Manuela Briere <manuelatgkb@gmail.com>
2021-10-21 11:52:06 -0300

    do while

--- src/modulo4/Aula25/View/Main.java
+++ src/modulo4/Aula25/View/Main.java
@@ -10,17 +10,33 @@ public class Main {
         PessoaFisica pf = new PessoaFisica();
         pf.id = 10;
         pf.nome = "Inex";
-        Endereco end1 = new Endereco();
+        do{
+            Endereco end1 = new Endereco();
+            end1.enderecoResidencial.logradouro = "Av Brasil";
+            end.enderecoResidencial.numero = "55";
+            end1.enderecoResidencial.complemento = "Apto 406b";
+            end1.enderecoResidencial.cep = "30140-130";
+            end1..enderecoResidencial.cidade = "Belo Horizonte";
+            end1.enderecoResidencial.estado = "Minas Gerais";
+            end1.enderecoResidencial.pais = "Brasil";
+            pf.enderecoResidencial = end1;
+        }while(true);
 
-        end1.enderecoResidencial.logradouro = "Av Brasil";
-        end.enderecoResidencial.numero = "55";
-        end1.enderecoResidencial.complemento = "Apto 406b";
-        end1.enderecoResidencial.cep = "30140-130";
-        end1..enderecoResidencial.cidade = "Belo Horizonte";
-        end1.enderecoResidencial.estado = "Minas Gerais";
-        end1.enderecoResidencial.pais = "Brasil";
-        pf.enderecoResidencial = end1;
         pf.enderecoComercial = end1;
+
+        System.out.println(pf.enderecoResidencial.logradouro);
+        System.out.println(pf.enderecoComercial.logradouro);
+
+        end1 = new Endereco();
+        System.out.println(end1.logradouro);
+        System.out.println(pf.enderecoResidencial.logradouro);
+        System.out.println(pf.enderecoComercial.logradouro);
+
+        System.out.println("Alteracoes");
+        end1.logradouro = "Av JK";
+
+        System.out.println(pf.enderecoResidencial.logradouro);
+
 
     }
 }
