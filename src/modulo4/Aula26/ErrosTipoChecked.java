207432d41b62a139d5a76c1c6c64e2c154730a25
Manuela Briere <manuelatgkb@gmail.com>
2021-10-22 17:55:12 -0300

    Erros tipo checked

---
+++ src/modulo4/Aula26/ErrosTipoChecked.java
@@ -0,0 +1,15 @@
+package modulo4.Aula26;
+
+import java.io.File;
+import java.io.IOException;
+
+public class ErrosTipoChecked {
+    public static void main(String[] args) {
+        File arquivo = new File("teste.txt");
+        try {
+            arquivo.createNewFile();
+        } catch (IOException e) {
+            System.out.println("Deu ruim");        
+        }
+    }
+}
