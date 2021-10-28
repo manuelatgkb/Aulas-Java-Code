db495d45fb61da2a4bcebc751338738d0cc73fd6
Manuela Briere <manuelatgkb@gmail.com>
2021-10-28 14:36:31 -0300

    script2

---
+++ src/ACJAAULASWEB/aula008/js/script2.js
@@ -0,0 +1,38 @@
+var pessoas= [];
+
+window.onload = function(){
+    console.log('Carregando JS')
+    carregaTabela();
+}
+
+function salvar(event){
+    event.preventDefault();
+    console.log('Salvando dados');
+    let nome = document.getElementById('nome');
+    let sobrenome = document.getElementById('sobrenome');
+    let idade = document.getElementById('idade');
+    //console.log(nome.value, sobrenome.value, idade.value);
+
+    //idades.push(idade.value);
+
+    //criando um dicionário, "chave" : valor.
+    if(idade.value >= 18){
+        pessoa = {"nome": nome.value, "sobrenome": sobrenome.value, "idade": idade.value};
+        localStorage.setItem("pessoa", pessoa);
+    }else{
+        alert('Sai fora de menor')
+    }
+    carregaTabela();
+    
+   // console.log(pessoa["nome"]);
+    console.log(pessoas);
+}
+
+function carregaTabela(){
+    let tbody = document.getElementById("corpo-tabela");
+    tbody.innerHTML= "";
+    console.log(localStorage.getItem("pessoa"));
+    //pessoas.forEach(p => {
+       // tbody.innerHTML += "<tr>"+ + "<td>"+p["nome"] +"</td>" + "<td>" + p["sobrenome"]+" </td>" +"<td>" + p["idade"]+ "</td>"+ "<tr/>"
+   // });
+}
