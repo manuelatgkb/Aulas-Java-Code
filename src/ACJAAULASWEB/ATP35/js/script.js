1b9cbc6551523b4ce006d9a8e06ca377f0c555b0
Manuela Briere <manuelatgkb@gmail.com>
2021-10-29 18:15:13 -0300

    v

--- src/ACJAAULASWEB/ATP35/js/script.js
+++ src/ACJAAULASWEB/ATP35/js/script.js
@@ -1,22 +1,27 @@
-function aboutus(){
+function about(){
     console.log("About us")
     let frame = document.getElementById("frame");
     frame.src = "about.html"
 
 }
 
-function whatcanwedo(){
+function services(){
     console.log("what we can do")
     let frame = document.getElementById("frame");
     frame.src = "services.html"
-
-
 }
 
 function contact(){
     console.log("Contato")
     let frame = document.getElementById("frame");
     frame.src = "contact.html"
+}
+function home(){
+    console.log("Home")
+    let frame = document.getElementById("frame");
+    frame.src = "home.html"
+}
 
-
+window.onload= function(){
+    home();
 }
