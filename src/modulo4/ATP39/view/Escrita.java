323ebce79a182c705ece90bf331907fee393466f
Manuela Briere <manuelatgkb@gmail.com>
2021-11-03 17:51:32 -0300

    Crie uma estrutura para escrita em arquivo. A estrutura deve conter um fluxo de saída de dados para arquivo, um forma de escrita no fluxo e um buffer de escrita. Com o buffer, escreva algumas linhas dentro do arquivo texto.

--- src/modulo4/ATP39/view/Escrita.java
+++ src/modulo4/ATP39/view/Escrita.java
@@ -1,5 +1,29 @@
 package modulo4.ATP39.view;
 
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
 public class Escrita {
-    
+    public static void main(String[] args) {
+        
+    try{
+        FileOutputStream fos = new FileOutputStream("dados/arquivo2.txt");
+        OutputStreamWriter osw = new OutputStreamWriter(fos);
+        BufferedWriter bw = new BufferedWriter(osw);
+
+        bw.write("Algumas Linhas");
+        bw.newLine();
+        bw.write("Mais algumas linhas");
+
+        bw.close();
+
+        }catch(FileNotFoundException e){
+            System.out.println("Não foi possível encontrar o arquivo.");
+        }catch(IOException e){
+            System.out.println("Não é possível escrever no arquivo.");
+        }
+    }
 }
