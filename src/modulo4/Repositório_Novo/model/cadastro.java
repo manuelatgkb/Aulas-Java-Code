4313157a0398bf8809b46a61dbc24e4e433a4055
Manuela Briere <manuelatgkb@gmail.com>
2021-11-03 11:21:57 -0300

    v

---
+++ src/modulo4/Repositório_Novo/model/cadastro.java
@@ -0,0 +1,11 @@
+package Repositório_Novo.model;
+
+public class cadastro extends abstrataId{
+    public String prato;
+    public Float preco;
+
+    public Prato(){
+        prato = new Prato();
+    }
+
+}
