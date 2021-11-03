5df664a4a2d588691ef05c9d8b506e9570c1ca7a
Manuela Briere <manuelatgkb@gmail.com>
2021-11-03 17:55:18 -0300

    Utilize o buffer de leitura como fonte de entrada para o buffer de escrita, ou seja, a entrada será o terminal e a saída o arquivo texto.

--- src/modulo4/ATP39/view/Leitura.java
+++ src/modulo4/ATP39/view/Leitura.java
@@ -1,23 +1,43 @@
 package view;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.Scanner;
 
 public class Leitura{
 
     public static void main(String[] args) {    
-            try {
-                Scanner sc = new Scanner(new File("P3/dados/molho.txt"));
-                while(sc.hasNextLine()){
-                    System.out.println(sc.nextLine());
-                }
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
+        try {
+            Scanner sc = new Scanner(new File("P3/dados/molho.txt"));
+            while(sc.hasNextLine()){
+                System.out.println(sc.nextLine());
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        try{
+            FileOutputStream fos = new FileOutputStream("dados/arquivo2.txt");
+            OutputStreamWriter osw = new OutputStreamWriter(fos);
+            BufferedWriter bw = new BufferedWriter(osw);
+    
+            bw.write("Algumas Linhas");
+            bw.newLine();
+            bw.write("Mais algumas linhas");
+    
+            bw.close();
+    
+            }catch(FileNotFoundException e){
+                System.out.println("Não foi possível encontrar o arquivo.");
+            }catch(IOException e){
+                System.out.println("Não é possível escrever no arquivo.");
         }
     }
 }
