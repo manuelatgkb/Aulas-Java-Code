41d4890a82a7e5a08c8929036922e1f5dcb80971
Manuela Briere <manuelatgkb@gmail.com>
2021-11-03 11:42:00 -0300

    como ler qualquer fluxo de entrada

--- src/modulo4/Aula28/view/Main.java
+++ src/modulo4/Aula28/view/Main.java
@@ -1,8 +1,35 @@
 package view;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
     public static void main(String[] args) {
         //java.io
-        
+
+        // criando o fluxo de entrada através de um arquivo
+        try{
+            FileInputStream fis = new FileInputStream("dados/pratos.txt");
+            //System.out.println(fis.read());
+            InputStreamReader isr = new InputStreamReader(fis);
+            //char[]dados = new char[5];
+            //isr.read(dados, 0, 5);
+            //System.out.println(dados);
+            BufferedReader br = new BufferedReader(isr);
+            while(br.ready()){
+                String linha = br.readLine();
+                System.out.println(linha);
+            }
+            br.close();
+
+            System.out.println("Arquivo aberto com sucesso!");
+        }catch(FileNotFoundException e){
+            System.out.println("Arquivo não encontrado.");
+        }catch(IOException e){
+            System.out.println("Não foi possível ler o arquivo.");
+        }
     }
 }
