cb51d563a7eafac5ad5baaf210e66c3c46366e69
Manuela Briere <manuelatgkb@gmail.com>
2021-11-03 14:11:10 -0300

    v

--- src/modulo4/ATP38/view/Escrita.java
+++ src/modulo4/ATP38/view/Escrita.java
@@ -3,7 +3,6 @@ import java.io.BufferedWriter;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 
 public class Escrita {
