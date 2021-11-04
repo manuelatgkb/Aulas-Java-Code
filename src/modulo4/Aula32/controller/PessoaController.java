7802a02238fa84b7c8d1f19592af50f99a2114d3
Manuela Briere <manuelatgkb@gmail.com>
2021-11-04 11:41:55 -0300

    dao

---
+++ src/modulo4/Aula32/controller/PessoaController.java
@@ -0,0 +1,6 @@
+package modulo4.Aula32.controller;
+import model.controller;
+
+public class PessoaController extends BaseController<Pessoa>{
+    
+}
---
+++ src/modulo4/Aula32/view/PessoaView.java
@@ -0,0 +1,17 @@
+import modulo4.Aula32.controller.PessoaController;
+
+public class PessoaView{
+    public static void main(String[] args) {
+        PessoaController pc = new PessoaController();
+        Pessoa p1 = new Pessoa();
+        p1.id = 1;
+        p1.nome = "Lala";
+        p1.sobrenome = "Brasil";
+
+        pc.create(p1);
+
+        for (Pessoa p: pc.read()) {
+            System.out.println(p);
+        }
+    }
+}
