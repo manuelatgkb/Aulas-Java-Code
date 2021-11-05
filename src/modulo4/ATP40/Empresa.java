0ca1ab802678e385b5a170e30f8d906bc0f8c3f7
Manuela Briere <manuelatgkb@gmail.com>
2021-11-05 16:11:45 -0300

    inicial

---
+++ src/modulo4/ATP40/Empresa.java
@@ -0,0 +1,7 @@
+package modulo4.ATP40;
+
+public class Empresa {
+    public String nome;
+    public String cnpj;
+    public String ramo;
+}
---
+++ src/modulo4/ATP40/IPontoTuristicoCrud.java
@@ -0,0 +1,8 @@
+package modulo4.ATP40;
+
+import java.util.ArrayList;
+
+public class IPontoTuristicoCrud {
+    public String save(PontoTuristico model);
+    public ArrayList<PontoTuristico> read();
+}
