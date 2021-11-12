d67fe92c00a654b3768c66eb31a1daaf3811d972
Manuela Briere <manuelatgkb@gmail.com>
2021-11-12 16:14:20 -0300

    v

---
+++ src/modulo4/ATP49/Create.java
@@ -0,0 +1,31 @@
+package src.modulo4.ATP49;
+
+public class Create{
+    import java.beans.Statement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+    public static void main(String[] args) {
+        try {
+            Connection conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres", "postgres", "123456");
+
+            PreparedStatement prepstatement = conn.prepstatement(sql);
+            String sql = "INSERT INTO categoria(nome, descricao) VALUES('nome', 'id')";
+            prepstatement.execute(sql, prepstatement.RETURN_GENERATED_KEYS);
+            ResultSet info = prepstatement.getGeneratedKeys();
+
+            while(info.next()){
+                int id = info.getInt("id");
+                System.out.println(id);
+            }
+
+            conn.close();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+    } 
+    
+}
