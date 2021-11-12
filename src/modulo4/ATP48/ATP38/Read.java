535585e6953632408b9e93b412d25017f59aa71b
Manuela Briere <manuelatgkb@gmail.com>
2021-11-12 12:12:32 -0300

    v

---
+++ src/modulo4/ATP48/ATP38/Read.java
@@ -0,0 +1,32 @@
+package src.modulo4.ATP48.ATP38;
+
+import java.beans.Statement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class Read {
+    public static void main(String[] args) {
+        try {
+            Connection conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres", "postgres", "123456");
+
+            PreparedStatement prepStatement = conn.prepareStatement(sql);
+            String sql = "SELECT * FROM categoria";
+            prepStatement.execute(sql);
+            ResultSet result = prepStatement.getResultSet();
+
+            while(result.next()){
+                int id = result.getInt("id");
+                String nome = result.getString("nome");
+                System.out.printf("%d: %s\n", id, nome);
+            }
+
+            conn.close();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
