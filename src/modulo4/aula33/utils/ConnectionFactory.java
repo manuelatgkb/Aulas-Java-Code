90c164e74a7cb629d4c7f487b5bc866103db878e
Manuela Briere <manuelatgkb@gmail.com>
2021-11-12 14:31:51 -0300

    ComboPooleDataSource

--- src/modulo4/aula33/utils/ConnectionFactory.java
+++ src/modulo4/aula33/utils/ConnectionFactory.java
@@ -4,15 +4,25 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+import javax.sql.DataSource;
+
 public class ConnectionFactory {
     private String url;
     private String user;
     private String password;
+    private DataSource dataSource;
 
 public ConnectionFactory(){
     this.url = "jdbc:postgresql://localhost:5432/postgres";
     this.user = "postgres";
     this.password = "123456";
+    ComboPooleDataSource pool = new ComboPooleDataSource();
+    pool.setjdbcURL(this.url);
+    pool.setUser(this.user);
+    pool.setPassword(this.password);
+    pool.setMaxPoolSize(10);
+    this.dataSource = pool;
+
 }
 
         public Connection getConnection() throws SQLException{
