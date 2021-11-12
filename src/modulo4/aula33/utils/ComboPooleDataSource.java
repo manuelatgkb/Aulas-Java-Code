658ae0238f5df3168879ea26c647f50cf90b1fe1
Manuela Briere <manuelatgkb@gmail.com>
2021-11-12 14:31:43 -0300

    ComboPooleDataSource

---
+++ src/modulo4/aula33/utils/ComboPooleDataSource.java
@@ -0,0 +1,17 @@
+package src.modulo4.aula33.utils;
+
+public class ComboPooleDataSource {
+
+    public void setjdbcURL(String url) {
+    }
+
+    public void setUser(String user) {
+    }
+
+    public void setPassword(String password) {
+    }
+
+    public void setMaxPoolSize(int i) {
+    }
+
+}
