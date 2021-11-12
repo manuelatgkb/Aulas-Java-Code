c855f883da2df0dba2d771ed010150494f31dd98
Manuela Briere <manuelatgkb@gmail.com>
2021-11-12 17:29:39 -0300

    v

---
+++ src/modulo4/ATP50/model/Categoria.java
@@ -0,0 +1,21 @@
+package src.modulo4.ATP50.model;
+
+public class Categoria {
+    private int id;
+    private String nome;
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getNome() {
+        return nome;
+    }
+}
