f9fccf61e900dcfdd28db6e74522765022a0bc76
Manuela Briere <manuelatgkb@gmail.com>
2021-11-15 14:53:00 -0300

    b

--- src/modulo4/aula36/view/View3Insert.java
+++ src/modulo4/aula36/view/View3Insert.java
@@ -7,29 +7,19 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-import modulo3.Aula20.MVC.Model.Categoria;
-import src.modulo4.aula33.utils.ConnectionFactory;
+import modulo4.aula36.model.Categoria;
+import modulo4.aula36.utils.CategoriaDao;
 
 public class View3Insert {
     public static void main(String[] args) {
-        try(Connection conn = new ConnectionFactory().getConnection()){
-            Categoria cat1 = new Categoria("TestePrepared");
 
-            
-            String sql = "INSERT INTO categoria(nome)values(?)";
-            PreparedStatement prepStatement = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
-            prepStatement.setString(1, nome);
-            
-            prepStatement.execute();
-            ResultSet ids = prepStatement.getGeneratedKeys();
+       try(Connection conn = new modulo4.aula36.utils.ConnectionFactory().getConnection()){
+           CategoriaDao dao = new CategoriaDao(conn);
+           Categoria cat1 = new Categoria("Teste1Dao");
 
-            while(ids.next()){
-                int id = ids.getInt("id");
-                System.out.println(id);
-            }
-
-        }catch(SQLException e){
-            e.printStackTrace();
-        }
+           dao.create(cat1);
+       }catch(SQLException e){
+           e.printStackTrace();
+       }
     }
 }
