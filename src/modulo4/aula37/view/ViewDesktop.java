10599226132e30c9cd0622fe64aa6195697067eb
Manuela Briere <manuelatgkb@gmail.com>
2021-11-16 17:14:53 -0300

    alterações

---
+++ src/modulo4/aula37/view/ViewDesktop.java
@@ -0,0 +1,141 @@
+package modulo4.aula37.view;
+
+import java.awt.Container;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JTable;
+import javax.swing.JTextField;
+import javax.swing.table.DefaultTableModel;
+
+import modulo3.ExerciciosExtra.R10.Domain.Categoria;
+import modulo4.aula37.view.dao.CategoriaDao;
+
+public class ViewDesktop extends JFrame{
+private CategoriaDao dao;
+private Container container;
+private JLabel lbNome;
+private JTextField txtNome;
+private JButton btnSalvar; btnDelete;
+private JTable table;
+private DefaultTableModel tableModel;
+
+
+    public ViewDesktop(){
+        super("Categorias");
+        setLayout(null);
+
+        inicializaComponentes();
+        posicionamento();
+        
+        adicionaEventos();
+
+        ler();
+        setSize(800, 600);
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
+
+    private void inicializaComponentes(){
+        dao = new CategoriaDao();
+        container = getContentPane();
+        lbNome = new JLabel("Nome:");
+        txtNome = new JTextField();
+        btnSalvar = new JButton("Salvar");
+        btnDelete = new JButton("Deletar");
+        table = new JTable();
+        tableModel = (DefaultTableModel)table.getModel();
+        tableModel.addColumn("id");
+        tableModel.addColumn("nome");
+        }
+
+    private void posicionamento(){
+        lbNome.setBounds(10,10,50,20);
+        txtNome.setBounds(70, 10, 250, 20);
+        btnSalvar.setBounds(10, 40, 100, 20);
+        table.setBounds(10, 80, 700, 300);
+        btnDelete.setBounds(10, 400, 100, 20);
+
+
+        container.add(lbNome);
+        container.add(txtNome);
+        container.add(btnSalvar);
+        container.add(table);
+        container.add(btnDelete);
+    }
+
+    private void adicionaEventos(){
+        btnSalvar.addActionListener(new aActionListener()){
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Categoria model = new Categoria();
+                model.setNome(txtNome.getText());
+                salvar(model);
+            }
+        }
+    }
+
+    lbNome.setBounds(10,10,50,20);
+        txtNome.setBounds(70, 10, 250, 20);
+        txtNome.setBounds(10, 40, 100, 20);
+        table.setBounds(10, 80, 700,300);
+
+
+        btnSalvar.addActionListener(new aActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e){
+                Categoria model = new Categoria();
+                model.setNome(txtNome.getText());
+                salvar(model);
+            }
+        });
+
+        btnDelete.addActionListener(new aActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e){
+                int id = (int)table.getValueAt(table.getSelectedRow(), 0);
+                Categoria model = new Categoria();
+                model.set(id);
+                delete(model);
+                JOptionPane.showMessageDialog(container, "Categoria de id :  "+ model.getId() + "Deletado com sucesso!");
+            }
+        });
+    }
+
+
+    btnUpdate.addActionListener(new aActionListener(){
+        @Override
+        public void actionPerformed(ActionEvent e){
+            int id = (int)table.getValueAt(table.getSelectedRow(), 0);
+            Categoria model = new Categoria();
+            model.setId(id);
+            model.setNome("nome");
+            alterar(model);
+            JOptionPane.showMessageDialog(container, "Categoria de id :  "+ model.getId() + "Deletado com sucesso!");
+        }
+    });
+}
+
+//==================================CRUD========================================
+    private void salvar(Categoria model){
+        dao.insert(model);
+        JOptionPane.showMessageDialog(container, "Categoria:" + model.getNome()+ "Salva com sucesso!");
+        ler();
+    }
+
+    private void ler(){
+        tableModel.setRowCount(0);
+        for (Categoria c : dao.read()) {
+            tableModel.addRow(new Object[]{c.getId(), c.getNome()});
+        }
+
+    }
+
+    private void delete(Categoria model){
+        dao.delete(model);
+        JOptionPane.showMessageDialog(container, "Categoria:" + model.getId()+ "Deletado com sucesso");
+        ler();
+    }
+}
--- src/modulo4/aula37/view/dao/CategoriaDao.java
+++ src/modulo4/aula37/view/dao/CategoriaDao.java
@@ -1,10 +1,19 @@
 package modulo4.aula37.view.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+import modulo4.aula37.utils.Categoria;
+import modulo4.aula37.utils.ConnectionFactory;
 
 public class CategoriaDao {
     public static void main(String[] args) {
-        public void insert(Categoria model){
+        public int insert(Categoria model){
+            int idGerado = 0;
             try(Connection conn = new ConnectionFactory().getConnection()){
                 
                 String sql = "INSERT INTO categoria(nome)values(?)";
@@ -15,12 +24,56 @@ public class CategoriaDao {
                 ResultSet ids = prepStatement.getGeneratedKeys();
     
                 while(ids.next()){
-                    int id = ids.getInt("id");
-                    System.out.println(id);
+                    idGerado = ids.getInt("id");
                 }
     
             }catch(SQLException e){
                 e.printStackTrace();
+            }
+            return idGerado;
         }
+        
+
+            public ArraList<Categoria> read(){
+                ArrayList<Categoria> list = new ArrayList<Categoria>();
+                try(Connection conn = new ConnectionFactory().getConnection()){
+                    PreparedStatement prepStatement = conn preparedStatement("SELECT * FROM categoria");
+                    prepStatement.executeQuery();
+                    ResultSet result = prepStatement.getResultSet();
+                    while(result.next()){
+                        Categoria model = new Categoria();
+                        model.setId(result.getInt("id"));
+                        model.setNome(result.getString("nome"));
+                        list.add(model);
+                    }
+                }catch (SQLException e){
+                    e.printStackTrace();
+                }
+                return list;
+            }
+        }
+            public int delete(Categoria model){
+                int linhasAfetadas = 0;
+                try(Connection conn = new ConnectionFactory().getConnection())
+                {
+                    Categoria model = new Categoria();
+                    model.setId(2);
+                    
+                    String sql = "DELETED * FROM categoria WHERE id = ?";
+        
+                    try(PreparedStatement prepstatement = conn.prepareStatement(sql))
+                    {
+                    prepstatement.setInt(1, model.getId());
+                    prepstatement.execute();
+                    linhasAfetadas = prepstatement.getUpdateCount();
+                
+                    }catch(SQLException e){
+                    e.printStackTrace();
+                    }
+                }catch(SQLException e){
+                    e.printStackTrace();
+                }
+                return linhasAfetadas;
     }
 }
+
