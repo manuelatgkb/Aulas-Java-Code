8a537f531e3954be325b171d258afec92f13b55f
Manuela Briere <manuelatgkb@gmail.com>
2021-11-19 10:53:20 -0300

    ATP52

---
+++ modulo4/ATP52/src/main/java/com/capgemini/manu/ATP52/App.java
@@ -0,0 +1,19 @@
+package com.capgemini.manu.ATP52;
+
+import javax.swing.JFrame;
+
+import com.capgemini.manu.ATP52.Model.Categoria;
+import com.capgemini.manu.ATP52.View.View;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	public static void main( String[] args )
+    {
+    	View w2 = new View();
+    	w2.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
---
+++ modulo4/ATP52/src/main/java/com/capgemini/manu/ATP52/Model/Categoria.java
@@ -0,0 +1,19 @@
+package com.capgemini.manu.ATP52.Model;
+
+public class Categoria {
+	private int id;
+	private String nome;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+}
