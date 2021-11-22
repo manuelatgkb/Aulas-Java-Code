3c6a28b1631c60ad46cb28632359cc131cc5e40b
Manuela Briere <manuelatgkb@gmail.com>
2021-11-22 15:47:30 -0300

    atp54 final

--- src/modulo4/ATP54/atp54/src/main/java/Produto.java
+++ src/modulo4/ATP54/atp54/src/main/java/Produto.java
@@ -8,10 +8,12 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 @WebServlet(urlPatterns = "/produto")
-
 public class Produto extends HttpServlet{
+
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp)throws ServletException, IOException{
+        PrintWriter out = resp.getWriter();
+
         String nome = req.getParameter("nome");
         String valor = req.getParameter("valor");
         String id_cat = req.getParameter("id_categoria");
@@ -20,10 +22,8 @@ public class Produto extends HttpServlet{
             float valor_produto = Float.parseFloat(valor);
             int id_categoria = Integer.parseInt("id_categoria");
 
-            PrintWriter out = resp.getWriter();
-            out.printf("Nome do Produto: %s", nome, "valor: %s", valor, "id da categoria: %s: ", id_cat);
-    }
-        else{
+            out.printf("Nome do Produto: %s", nome, "valor: %s", valor_produto, "id da categoria: %s: ", id_categoria);
+        }else{
             out.printf("Nome do Produto: %s", nome);
         }
     }
