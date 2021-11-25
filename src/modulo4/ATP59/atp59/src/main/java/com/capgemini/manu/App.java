e065c59266bfc5bd9d416358b281eb4109d3185a
Manuela Briere <manuelatgkb@gmail.com>
2021-11-25 16:59:53 -0300

    atp59 configuracoes iniciais

---
+++ src/modulo4/ATP59/atp59/src/main/java/com/capgemini/manu/App.java
@@ -0,0 +1,13 @@
+package com.capgemini.manu;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
