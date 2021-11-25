fab8e39bc82e077f54069e286c8658cbe3d6dcdc
Manuela Briere <manuelatgkb@gmail.com>
2021-11-25 17:21:43 -0300

    atp59 final

--- src/modulo4/ATP59/atp59/src/main/java/com/capgemini/manu/App.java
+++ src/modulo4/ATP59/atp59/src/main/java/com/capgemini/manu/App.java
@@ -1,6 +1,13 @@
 package com.capgemini.manu;
 
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 import com.capgemini.manu.model.Categoria;
+
 
 /**
  * Hello world!
@@ -14,6 +21,17 @@ public class App
         Categoria model = new Categoria();
         model.setNome("Testes JPA");
 
+        EntityManagerFactory factory = Persistence.createEntityManagerFactory("banco");
+        EntityManager em = factory.createEntityManager();
+
+        em.getTransaction().begin();
+        em.persist(model);
+        em.getTransaction().commit();
+
+        List<Categoria> lista = em.createQuery("SELECT c FROM Categoria c", Categoria.class).getResultList();
         
+        for (Categoria categoria : lista) {
+            System.out.printf("%d - %s\n",categoria.getId(),categoria.getNome());
+        }
     }
 }
