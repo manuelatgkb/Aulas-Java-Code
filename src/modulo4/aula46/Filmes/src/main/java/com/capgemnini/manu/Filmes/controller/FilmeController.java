a40e23e285e09b3b74847d06fd1508a1d6c072ec
Manuela Briere <manuelatgkb@gmail.com>
2021-11-30 11:26:56 -0300

    atp65

--- src/modulo4/aula46/Filmes/src/main/java/com/capgemnini/manu/Filmes/controller/FilmeController.java
+++ src/modulo4/aula46/Filmes/src/main/java/com/capgemnini/manu/Filmes/controller/FilmeController.java
@@ -1,26 +1,20 @@
-package com.capgemnini.manu.Filmes.controller;
+import java.util.List;
 
-import javax.servlet.http.HttpServletRequest;
-
-import com.capgemnini.manu.Filmes.model.Filme;
-
-import org.apache.catalina.startup.ClassLoaderFactory.Repository;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-
-import repository.FilmeRepository;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
-public class FilmeController(){
-    
-    @GetMapping("/filme")
+	public FilmeController(FilmeRepository repository) {
+    }
+
+    @GetMapping("/model/Filme")
     public String filmes(Model req){
         List<Filme> lista = (List<Filme>)repository.findAll();
-        req.addAttribute("filmes", lista);
+        req.addAttribute("Filmes", lista);
         return "filmes";
 
     }
@@ -32,21 +26,31 @@ public class FilmeController(){
         return "filmes-form";
     }
 
-	@PostMapping public String salvar(Filme model){
-        Repository.save(model);
+	@PostMapping("/filme/salvar")
+    public String salvar(@RequestBody Filme model){
+        repository.save(model);
         return "redirect:/filme";
     }
+    
 
     @GetMapping("/filme/deletar/{id}")
     public String deletar(@PathVariable int id){
-        Repository.deleteById(id);
+        repository.deleteById(id);
         return "redirect:/filme";
     }
 
     @GetMapping("/filme/editar/{id}")
     public String editar(@PathVariable int id, Model req){
-        Filme model = Repository.findById(id).get();
+        Filme model = repository.findById(id).get();
         req.addAttribute("filme", model);
         return "filmes-form";
+    }
+    @PostMapping("/api/filme/{id}")
+    public String update(@RequestBody Filme model, @PathVariable int id){
+        if(model.getId() == id){
+        repository.save(model);
+        return "redirect:/filme";
+        }
+    return "id da url diferente do body";
     }
 }
