25166e21ba6f5204905991690a3804b5448d3552
Manuela Briere <manuelatgkb@gmail.com>
2021-12-01 14:05:43 -0300

    aula js parte 2

--- src/modulo4/aula003JS/04-listas.js
+++ src/modulo4/aula003JS/04-listas.js
@@ -1,3 +1,24 @@
 const lista = ["Inez", "Jojo", "Guga"]
+const lista2 = new Array("Inex", "Jojo", "guga");
 
 console.log(lista);
+console.log(lista2);
+
+lista.push("Chimbinha");
+// lista = []; uma lsita const permite adicao de itens numa lista mas nao permite alteracao dos valores da lista.
+console.log(lista);
+console.log(lista.length);
+console.log("Index da JOJO", lista.indexOf("Jojo"));
+
+const valor = lista.pop();
+console.log(valor);
+console.log(lista);
+
+const novaLista = lista.slice(1,2);
+console.log(novaLista);
+
+lista.splice(1,1);
+console.log(lista);
+
+
+
