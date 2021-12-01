30712a5cb5f318612199286a823f1a5201aa9466
Manuela Briere <manuelatgkb@gmail.com>
2021-12-01 11:58:40 -0300

    aula001JS

---
+++ src/modulo4/aula001JS/js/variaveis.js
@@ -0,0 +1,11 @@
+//let, var
+
+const nome= "Maykon";
+const salario = 1950.09;
+const idade = 18;
+
+
+console.log(nome);
+console.log(idade);
+console.log(salario);
+console.log(nome, idade, salario);
