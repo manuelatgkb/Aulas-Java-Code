78c7386e80447ed5670dacb56d8d4e46832c7cc7
Manuela Briere <manuelatgkb@gmail.com>
2021-12-01 11:58:49 -0300

    Js

---
+++ src/modulo4/aula001JS/js/conversoes.js
@@ -0,0 +1,15 @@
+//JS é dinamicamente tipada, ela defini o tipo ao receber o valor
+//Java é estaticamente tipada, precisa definir o tipo ao declarar a varável
+
+const nome = "inex";
+const idade = 50;
+const salario = 1950.55;
+
+console.log(nome + idade);
+console.log(salario + idade); //ele soma as variáveis apesar de elas não serem iguais, provacando i=um erro.
+console.log("10" + "20");
+console.log(parseInt("10") + parseInt("20"));
+
+console.log("10" /"20");
+console.log("10" - "20");
+
