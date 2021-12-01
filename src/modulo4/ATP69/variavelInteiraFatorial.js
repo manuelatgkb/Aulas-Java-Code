917e05c2e2ca48f54a8e196d12eb2d045e1a7fa6
Manuela Briere <manuelatgkb@gmail.com>
2021-12-01 14:50:14 -0300

    ATP69Final

--- src/modulo4/ATP69/variavelInteiraFatorial.js
+++ src/modulo4/ATP69/variavelInteiraFatorial.js
@@ -1,5 +1,11 @@
 //Crie um arquivo adicione uma variável inteira com qualquer valor. Calcule o fatorial deste valor utilizando o for e imprima o resultado no terminal.
 
+var fatorial = 5;
+var resultado = fatorial;
+for (var i = 1; i < fatorial; i++) {
+    resultado *= i;
+}
+console.log(resultado);
 
 
 // Crie um arquivo adicione uma lista de nomes. Imprima o valor dos elementos no terminal utilizando a função forEach.
