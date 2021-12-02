a0aa2f93dfe9865169526011bd23caa1dc5e10c9
Manuela Briere <manuelatgkb@gmail.com>
2021-12-02 10:30:28 -0300

    ATP71

---
+++ src/modulo4/ATP71/categoria.js
@@ -0,0 +1,13 @@
+export class categoria{
+    descricao;
+    IdProduto;
+    
+ 
+    get categoria(){
+        return this.categoria;
+    }
+
+    set categoria(cat){
+        this.categoria = cat;
+    }
+}
